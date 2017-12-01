a7245e89e7fc6bbcb008271f51bcd2de18fa132d
tarasen <tarasenkomax@i.ua>
2017-12-01 18:48:29 +0200

    Login application

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/tarasen/Studying/3kurs/kursova/Django-Agregator-Site/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
