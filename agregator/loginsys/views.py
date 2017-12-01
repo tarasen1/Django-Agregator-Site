a9435719c34dab4ff84c271c9c89635d9f30a4af
tarasen <tarasenkomax@i.ua>
2017-12-01 19:44:02 +0200

    Update

--- agregator/loginsys/views.py
+++ agregator/loginsys/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.contrib import auth
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def register(request):
+	return HttpResponse('register')
+
+def login(request):
+	return HttpResponse('login')
+
+def logout(request):
+	return HttpResponse('logout')
+
+
+
