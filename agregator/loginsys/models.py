81ddf801cc8ec7bb3b1909ecf54f25ee20c4929d
tarasen <tarasenkomax@i.ua>
2017-12-02 10:44:35 +0200

    Update

--- agregator/loginsys/models.py
+++ agregator/loginsys/models.py
@@ -1,3 +1,5 @@
 from django.db import models
+import hashlib
 
-# Create your models here.
+
+		
--- agregator/loginsys/views.py
+++ agregator/loginsys/views.py
@@ -4,13 +4,43 @@ from django.contrib import auth
 from django.contrib.auth.forms import UserCreationForm
 
 def register(request):
-	return HttpResponse('register')
+	context = {
+		'form'	:	UserCreationForm(),
+	}
+	if request.method == 'POST':
+		newuser_form = UserCreationForm(request.POST)
+		if newuser_form.is_valid():
+			newuser_form.save()
+			newuser = auth.authenticate(username=newuser_form.cleaned_data['username'], password=newuser_form.cleaned_data['password2'])
+			auth.login(request, newuser)
+			return redirect('/')
+		else:
+			context = {
+				'form'	:	newuser_form
+			}
+	return render(request, 'loginsys/register.html', context)
 
 def login(request):
-	return HttpResponse('login')
+	#return HttpResponse('login')
+	context = {}
+	if request.method == 'POST':
+		username = request.POST.get('username', '')
+		password = request.POST.get('password', '')
+		user = auth.authenticate(username=username, password=password)
+		if user is not None:
+			auth.login(request, user)
+			return redirect('/')
+		else:
+			context = {
+				'login_error'	:	'Incorrect login or password',
+			}
+			return render(request, 'loginsys/login.html', context)
+	else:
+		return render(request, 'loginsys/login.html', context)
 
 def logout(request):
-	return HttpResponse('logout')
+	auth.logout(request)
+	return redirect('/')
 
 
 
